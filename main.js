d1f8cef2279c103feb4165472bcda9afd4cfe994
Bruno Rodrigues Da Silva <Brunochh@outlook.com>
2023-01-01 23:01:22 -0300

    adição das músicas

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-let musicaPrincipal = document.querySelector('audio');
+/*Atualizar as informacoes do banner pincipal*/
+
+let musicaPrincipal = document.querySelector('.audioPrincipal');
+const chippin_in = document.querySelector('.playChippin');
+const never_Fade_Away = document.querySelector('.playNever_Fade_Away');
+const Testament = document.querySelector('.playTestament');
+const my_Boo = document.querySelector('.playmyBoo');
+const fairy_Tale = document.querySelector('.playfairyTale');
+const when_I_Was_Your_Man = document.querySelector('.playWhenIWasYourMan');
+
 
 document.querySelector('.play').addEventListener('click', tocarMusica);
 
@@ -6,8 +15,45 @@ document.querySelector('.play').addEventListener('click', tocarMusica);
         musicaPrincipal.play();
     }
 
-function musicasRecentes() {
-    if (tocarMusica == true) {
-        
+
+document.querySelector('.Chippin_In').addEventListener('click', tocarChippin);
+    function tocarChippin(){
+        musicaPrincipal = chippin_in
+        chippin_in.play();
+        document.getElementsByClassName('.nomeMusica') = 'Chippin`in';
     }
-}
+
+document.querySelector('.Never_Fade_Away').addEventListener('click', tocarNeverFade);
+    function tocarNeverFade(){
+        musicaPrincipal = never_Fade_Away;
+        never_Fade_Away.play();
+    }
+
+
+    document.querySelector('.Testament').addEventListener('click', tocarTestament);
+    function tocarTestament(){
+        musicaPrincipal = Testament
+        Testament.play();
+    }
+
+
+document.querySelector('.My_Boo').addEventListener('click', tocarmyBoo);
+    function tocarmyBoo(){
+        musicaPrincipal = my_Boo
+        my_Boo.play();
+    }
+
+
+document.querySelector('.Fairy_Tale').addEventListener('click', tocarfairyTale);
+    function tocarfairyTale(){
+        musicaPrincipal = fairy_Tale
+        fairy_Tale.play();
+    }
+
+
+    document.querySelector('.When_I_Was_Your_Man').addEventListener('click', tocarbrunoMars);
+    function tocarbrunoMars(){
+        musicaPrincipal = when_I_Was_Your_Man
+        when_I_Was_Your_Man.play();
+    }
+
